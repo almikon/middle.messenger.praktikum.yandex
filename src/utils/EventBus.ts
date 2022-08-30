b0432265fa02a4c4b2198f9718f48d76a746e55c
wwwaw <aold88@gmail.com>
2022-08-30 03:22:41 +0300

    struggling with TS

---
+++ src/utils/EventBus.ts
@@ -0,0 +1,29 @@
+export class EventBus {
+    private readonly listeners: Record<string, Array<() => void>> = {};
+
+    on(event: string, callback: () => void) {
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+        }
+
+        this.listeners[event].push(callback);
+    }
+
+    off(event: string, callback: () => void) {
+        if (!this.listeners[event]) {
+            throw new Error(`Нет события: ${event}`);
+        }
+
+        this.listeners[event] = this.listeners[event].filter(
+            listener => listener !== callback
+        );
+    }
+
+    emit(event: string, ...args: []) {
+        if (!this.listeners[event]) {
+            throw new Event(`Нет события: ${event}`);
+        }
+
+        this.listeners[event].forEach(listener => { listener(...args) });
+    }
+}
