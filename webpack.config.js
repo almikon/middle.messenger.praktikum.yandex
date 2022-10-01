b7fe66e21ff73d91117c75cb005eec2d0072f927
wwwaw <aold88@gmail.com>
2022-10-01 12:17:30 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.ts'
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            configFile: path.resolve(__dirname, 'tsconfig.json'),
+                        },
+                    },
+                ],
+                exclude: /(node_modules)/
+            },
+            {
+                test: /\.hbs$/,
+                use: [
+                    {
+                        loader: "handlebars-loader",
+                        options: {
+                            configFile: path.resolve(__dirname, 'tsconfig.json'),
+                        },
+                    },
+                ],
+                exclude: /(node_modules)/
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            configFile: path.resolve(__dirname, 'tsconfig.json'),
+                        },
+                    },
+                ],
+                exclude: /(node_modules)/
+            },
+            {
+                test: /\.less$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "less-loader",
+                ],
+                exclude: /(node_modules)/
+            }
+        ]
+    }
+};
