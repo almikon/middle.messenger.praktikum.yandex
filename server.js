eddc3978723c6cdbdf4f7586b89ada129a7ad82f
oldst <konyahin_al@mail.ru>
2022-10-07 23:47:32 +0300

    lint staged & heroku reload err

--- server.js
+++ server.js
@@ -8,3 +8,4 @@ app.use(express.static('./dist/'));
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
 }); 
+app.get('/*', (_, res) => res.send('/messenger'));
