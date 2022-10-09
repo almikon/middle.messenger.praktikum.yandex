28bbd842d21b61a23cbb5f45240e0bd8e56c9c34
oldst <konyahin_al@mail.ru>
2022-10-09 22:04:43 +0300

    heroku

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const fallback = require('express-history-api-fallback');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static('./dist/'));
-app.use(fallback('./dist/index.html', { root: __dirname }));
+const root = __dirname + '/dist';
+app.use(express.static(root));
+app.use(fallback('index.html', { root: root }));
 
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
